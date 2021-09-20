c722441e49f8f1a6491453cd1374ee54ca08f85c
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-20 20:55:37 +0200

    [add] footer styled elements

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FooterContainer} from "./FooterStyledElement";
+import {HeroSubTitle} from "./homepage/HomeStyledElements";
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <HeroSubTitle>Footer</HeroSubTitle>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
