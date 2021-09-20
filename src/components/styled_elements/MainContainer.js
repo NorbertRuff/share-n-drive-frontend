9dc53a33feeb368d8940baf14acc93e8c6a10f7b
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-20 20:54:05 +0200

    [add] main container

--- src/components/styled_elements/MainContainer.js
+++ src/components/styled_elements/MainContainer.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
- 
+  display: grid;
+  width: 100vw;
+  max-width: 100%;
+  height: 100vh;
+  grid-template-columns: 5% 90% 5%;
+  grid-template-rows: 4rem auto 4rem;
+  grid-template-areas:
+    "header header header"
+    "content content content"
+    "footer footer footer";
 `;
