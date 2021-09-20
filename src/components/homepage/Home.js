6546d548374fe8dd8672b73f6229b5be7388ebdc
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-20 20:53:22 +0200

    [add] hero containers

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {FeaturedCarsContainer, HeroContainer, HeroSubTitle, HeroTitle, HomeContainer} from "./HomeStyledElements";
+
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <HomeContainer>
+            <HeroContainer/>
+
+            <HeroTitle>Find your drive</HeroTitle>
+            <HeroSubTitle>Explore the world's largest car sharing marketplace</HeroSubTitle>
+            <FeaturedCarsContainer>
+
+
+            </FeaturedCarsContainer>
+
+        </HomeContainer>
     );
 };
 
