cae7eff68458fc010861fd1d44df518987a34b7d
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-20 20:55:25 +0200

    [add] navbar styled elements

---
+++ src/components/navbar/NavbarStyledElements.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import {NavLink} from "react-router-dom";
+
+
+export const LogoContainer = styled(NavLink)``;
+
+export const Logo = styled.div`
+`;
+
+export const NavLinks = styled.div``;
+
+export const UserLogo = styled.div``;
+
+export const NavBarContainer = styled.div`
+  grid-area: header;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  height: 5vh;
+  background-color: white;
+  border: 1px solid black;
+`;
