9415be44f3f171fe36dadd1a83ed3f49d701cfef
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 20:34:13 +0200

    [add] home structure

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import {
-    HeroContainer,
-    HeroSubTitle,
-    HeroTitle,
-    HomeContainer
-} from "./HomeStyledElements";
+import {HeroContainer, HeroSubTitle, HeroTitle, HomeContainer} from "./HomeStyledElements";
 import FeaturedContainer from "./FeaturedContainer";
 import FilteredContainer from "./FilteredContainer";
+import hero from "../../assets/img/new_mainbanner.jpg";
+import hero2 from "../../assets/img/hero.jpg";
 
 const Home = () => {
 
 
     return (
         <HomeContainer>
-            <HeroContainer/>
+            <HeroContainer image={hero}/>
             <HeroTitle>Find your drive</HeroTitle>
             <HeroSubTitle>Explore the world's largest car sharing marketplace</HeroSubTitle>
             <FeaturedContainer/>
-                <FilteredContainer/>
-            <HeroContainer/>
+            <FilteredContainer/>
+            <HeroContainer image={hero2}/>
             <HeroTitle>Share Your Car!</HeroTitle>
             <HeroSubTitle>Start here now!</HeroSubTitle>
         </HomeContainer>
