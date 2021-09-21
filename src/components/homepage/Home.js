e38d4fe0838f6aa54de49ba0934229d29ef45788
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 13:40:32 +0200

    [refactor] home page component

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,90 +1,26 @@
 import React from 'react';
 import {
-    FeaturedCarsContainer, FilterButtons,
-    FilterCars, FilteredCarsContainer, FilteredItems, FilterHeroTitle, FilterOption,
     HeroContainer,
     HeroSubTitle,
     HeroTitle,
-    HomeContainer, SelectOption
+    HomeContainer
 } from "./HomeStyledElements";
-import Select from 'react-select'
-import makeAnimated from 'react-select/animated';
-
-const animatedComponents = makeAnimated();
-
-const selectStyle = {
-    control: styles => ({ ...styles, backgroundColor: 'black' , color: 'white'}),
-
-
-}
-
-
-const ColorOptions = [
-    {value: 'chocolate', label: 'Chocolate'},
-    {value: 'strawberry', label: 'Strawberry'},
-    {value: 'vanilla', label: 'Vanilla'},
-    {value: 'silver', label: 'Silver'},
-    {value: 'black', label: 'Black'}
-]
-
-const BodyTypeOptions = [
-    {value: 'station_wagon', label: 'Station Wagon'},
-    {value: 'coupe', label: 'Coupe'},
-    {value: 'convertible', label: 'Convertible'}
-]
-
-const CarmakerOptions = [
-    {value: 'audi', label: 'Audi'},
-    {value: 'bmw', label: 'Bmw'},
-    {value: 'vw', label: 'Volkswagen'}
-]
+import FeaturedContainer from "./FeaturedContainer";
+import FilteredContainer from "./FilteredContainer";
 
 const Home = () => {
+
 
     return (
         <HomeContainer>
             <HeroContainer/>
-
             <HeroTitle>Find your drive</HeroTitle>
             <HeroSubTitle>Explore the world's largest car sharing marketplace</HeroSubTitle>
-            <FeaturedCarsContainer>
-                <h2>Featured cars</h2>
-
-            </FeaturedCarsContainer>
-            <FilterCars>
-                <FilterHeroTitle>Filter Cars</FilterHeroTitle>
-                <FilterButtons>
-                    <FilterOption>
-                        <h2>Color</h2>
-                        <Select closeMenuOnSelect={false}
-                                styles={selectStyle}
-                                components={animatedComponents}
-                                isMulti
-                                options={ColorOptions}/>
-                    </FilterOption>
-                    <FilterOption>
-                        <h2>Manufacturer</h2>
-                        <Select
-                            styles={selectStyle}
-                            closeMenuOnSelect={false}
-                            components={animatedComponents}
-                            isMulti
-                            options={CarmakerOptions}/>
-                    </FilterOption>
-                    <FilterOption>
-                        <h2>BodyType</h2>
-                        <Select
-                            closeMenuOnSelect={false}
-                            styles={selectStyle}
-                            components={animatedComponents}
-                            isMulti
-                            options={BodyTypeOptions}/>
-                    </FilterOption>
-                </FilterButtons>
-                <FilteredCarsContainer>
-
-                </FilteredCarsContainer>
-            </FilterCars>
+            <FeaturedContainer/>
+                <FilteredContainer/>
+            <HeroContainer/>
+            <HeroTitle>Share Your Car!</HeroTitle>
+            <HeroSubTitle>Start here now!</HeroSubTitle>
         </HomeContainer>
     );
 };
