d2bc7dbb30e968ee19e11dc2f1e620715026b7f8
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 20:33:54 +0200

    [fix] main container navbar height

--- src/components/styled_elements/MainContainer.js
+++ src/components/styled_elements/MainContainer.js
@@ -3,10 +3,8 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
   display: grid;
   width: 100vw;
-  max-width: 100%;
-  height: 100vh;
   grid-template-columns: 5% 90% 5%;
-  grid-template-rows:10vh auto 5rem;
+  grid-template-rows: 3rem auto 5rem;
   grid-template-areas:
     "header header header"
     "content content content"
