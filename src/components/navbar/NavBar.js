a2f5379c8510fe9b15b5530bf2eff4194cac196d
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 13:41:26 +0200

    [add] dropdown for navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {DropdownMenu, Logo, NavBarContainer, NavLinks, UserLogo} from "./NavbarStyledElements";
 
 
-import {Logo, NavBarContainer, NavLinks, UserLogo} from "./NavbarStyledElements";
 
 const NavBar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [isToggled, setIsToggled] = useState('none');
+
+
+    const dropMenu = () => {
+        if (showMenu) {
+            setShowMenu(false);
+        } else if (isToggled) {
+            setIsToggled('none');
+            setShowMenu(true);
+        } else {
+            setShowMenu(true);
+        }
+    };
+
+
     return (
         <NavBarContainer>
             <Logo data-testid="logo" to="/" title="Home"/>
             <div/>
             <NavLinks>links</NavLinks>
-            <UserLogo />
+            <UserLogo onClick={dropMenu}>
+                {showMenu ? (
+                    <DropdownMenu>
+                        <li>Profile</li>
+                        <li>Rent a car</li>
+                        <li>Share a car</li>
+                        <li>calendar</li>
+                    </DropdownMenu>
+                ) : null}
+
+
+            </UserLogo>
         </NavBarContainer>
     );
 };
