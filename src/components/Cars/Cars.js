0f79387b461a1e0f83fbe112375fcf17d4f37a09
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 21:08:46 +0200

    [add] dummy blank page

---
+++ src/components/Cars/Cars.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Cars = () => {
+    return (
+        <CarsContainer></CarsContainer>
+    );
+};
+
+export const CarsContainer = styled.div`
+  grid-area: content;
+
+`;
+
+
+export default Cars;
