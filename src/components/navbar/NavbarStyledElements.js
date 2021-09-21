da66b142a8bb43fe9d9a23541fd4fc9b65ecfa80
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-21 09:47:02 +0200

    [add] navbar styled element

--- src/components/navbar/NavbarStyledElements.js
+++ src/components/navbar/NavbarStyledElements.js
@@ -1,22 +1,33 @@
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
+import MainLogo from "../../assets/img/logo.png"
+import UserLogoPic from "../../assets/img/user.png"
 
 
-export const LogoContainer = styled(NavLink)``;
-
-export const Logo = styled.div`
+export const Logo = styled(NavLink)`
+  background-image: url(${MainLogo});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100%;
 `;
 
 export const NavLinks = styled.div``;
 
-export const UserLogo = styled.div``;
+export const UserLogo = styled.div`
+  padding: 1rem;
+  background-image: url(${UserLogoPic});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 80%;
+`;
 
 export const NavBarContainer = styled.div`
   grid-area: header;
-  display: flex;
-  justify-content: space-between;
-  padding: 1rem;
-  height: 5vh;
+  display: grid;
+  grid-template-columns: 10% auto 10% 10%;
+  align-items: center;
   background-color: white;
-  border: 1px solid black;
+  border-bottom: 3px solid var(--clr-primary-200);
 `;
