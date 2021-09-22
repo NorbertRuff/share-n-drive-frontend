e0fa33554fb4fa69f63a4b0d7bcfad252cf894a6
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-22 21:28:30 +0200

    [add] user styles

--- src/components/UserPage/UserPageStyledElements.js
+++ src/components/UserPage/UserPageStyledElements.js
@@ -2,30 +2,81 @@ import styled from "styled-components";
 
 
 export const UserProfileContainer = styled.div`
-  grid-area: content;
-  
+  grid-area: MainContent;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const UserProfileDetails = styled.div`
+  background-color: var(--clr-primary-200);
+  margin: 2rem auto;
+  padding: 1rem;
   display: grid;
-  
-  grid-template-areas: 
+  grid-template-columns: 0.6fr 2fr;
+  width: 80vw;
+  justify-content: space-between;
+  align-items: start;
+  justify-items: stretch;
+  grid-template-areas:
     "Avatar Name Name"
-    "Avatar Address Address";
-
+    "Avatar Details Details"
+    "Menu Details Details";
 `;
 
 export const UserAvatarDiv = styled.div`
-    grid-area: avatar;
+  grid-area: Avatar;
+  background-color: var(--clr-primary-400);
 `;
 export const UserAvatar = styled.img`
-    width: 30vw;
-    height: 30vw;
-    object-fit: contain;
+  width: 30vw;
+  height: 20vh;
+  object-fit: contain;
 `;
 export const UserName = styled.div`
   grid-area: Name;
+  text-align: center;
+  font-size: 2rem;
+  var(--ff-heading-bold);
 `;
-export const UserAddress = styled.div`
-  grid-area: Address;
+
+export const UserMenuItem = styled.div`
+  text-align: right;
+  font-size: 2rem;
+  padding: 1rem;
+  text-transform: uppercase;
+  border: 2px solid var(--clr-primary-100);
+`;
+export const UserMenu = styled.div`
+  grid-area: Menu;
+  background-color: var(--clr-primary-300);
+
+`;
+export const UserMenuItemTitle = styled.div`
+  font-size: 2rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--clr-primary-100);
+
+`;
+export const Details = styled.div`
+  grid-area: Details;
+`;
+
+export const UserCars = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  gap: 1vw;
+  background-color: var(--clr-primary-300);
+  margin: 3rem;
+
+`;
+
+export const UserCalendar = styled.div`
+  background-color: var(--clr-primary-300);
+  height: 30vh;
+  margin: 3rem;
+
 `;
