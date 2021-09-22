6884b718efae4a94ea92284fcf8952c95ed47b14
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-22 10:07:58 +0200

    [refactor] styling moved from HomeStyledElements.js

--- src/components/homepage/HomeStyledElements.js
+++ src/components/homepage/HomeStyledElements.js
@@ -41,54 +41,5 @@ export const FeaturedCarsContainer = styled.div`
   margin: 1rem;
   color: black;
 `;
-export const FilterCars = styled.div`
-  display: grid;
-  margin: 3rem 0;
-  background-color: white;
-  color: black;
-  grid-template-columns: 20vw 1fr;
-  grid-template-rows: 1fr 3fr 3fr;
-  grid-template-areas: 
-    "heroTitle heroTitle heroTitle"
-    "FilterButtons FilteredItems FilteredItems"
-    "FilterButtons FilteredItems FilteredItems";
-`;
-export const FilterButtons = styled.div`
-  grid-area:FilterButtons;
-`;
 
 
-export const FilterHeroTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  grid-area: heroTitle;
-  font-size: 3rem;
-  font-family: var(--ff-heading-light);
-  background-color: var(--clr-primary-300);
-  text-align: center;
-  padding: 1rem;
-  color: var(--clr-primary-500);
-
-`;
-
-
-export const FilterOption = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-
-  padding: 1rem;
-`;
-
-export const FilteredCarsContainer = styled.div`
-  padding: 1rem;
-  margin: 1rem;
-  display: flex;
-  grid-area: FilteredItems;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid var(--clr-primary-100);
-  height: 50vh;
-
-`;
