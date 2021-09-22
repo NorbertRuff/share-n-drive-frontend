cb0996a18336ecd49bab7f2218d8a833fcc38761
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-22 21:26:20 +0200

    [fix] test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import * as assert from "assert";
 
-beforeAll(() =>
-    Object.defineProperty(HTMLMediaElement.prototype, "muted", {
-        set: jest.fn(),
-    })
-);
 
 it('renders without crashing', () => {
-    assert(true)
+    expect(true).toBeTruthy();
 });
 
 
