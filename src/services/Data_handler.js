baaa152ed8f1f5b56c7d121e406c6a54dd98385d
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-23 20:14:29 +0200

    [add] data handler option getter fctn

--- src/services/Data_handler.js
+++ src/services/Data_handler.js
@@ -19,6 +19,23 @@ export let dataHandler = {
                 );
             });
     },
+    _api_get_selectOptions: function (url, callback, callbackOfTheCallback, errorCallback) {
+        axios
+            .get(url, {
+                method: 'GET',
+                credentials: 'same-origin'
+            })
+            .then((response) => {
+                callback(response.data, callbackOfTheCallback);
+            })
+            .catch((error) => {
+                errorCallback(error.message);
+                console.error(
+                    `The request was made and the server responded
+        with a status code that falls out of the range of 2xx ` + error.message
+                );
+            });
+    },
     _api_get_results: function (url, callback, errorCallback) {
         axios
             .get(url)
