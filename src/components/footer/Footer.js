664d9d6928584b369b93cda6dff447cf9290cb65
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-23 11:02:44 +0200

    [add] footer style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,8 @@ import {FooterContainer, FooterElements} from "./FooterStyledElement";
 const Footer = () => {
     return (
         <FooterContainer>
-            <FooterElements>Footer</FooterElements>
+            <FooterElements>A project created by:</FooterElements>
+            <FooterElements>Marta Kowalska | Norbert Ruff | Ferenc Kárpáti</FooterElements>
         </FooterContainer>
     );
 };
--- src/components/footer/FooterStyledElement.js
+++ src/components/footer/FooterStyledElement.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FooterContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-evenly;
   background-color: black;
   color: white;
   grid-area: footer;
