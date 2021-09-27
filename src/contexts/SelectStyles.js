4f8a8d7bf2b8bb0255ee4c6c26b9d503d3034765
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-27 15:11:17 +0200

    [add] selector styles

---
+++ src/contexts/SelectStyles.js
@@ -0,0 +1,74 @@
+export const selectStyle = {
+    control: styles => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: 'var(--clr-primary-200)',
+        color: 'var(--clr-primary-500)',
+        fontSize: '1.2rem',
+        padding: '0.3rem',
+    }),
+    option: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        fontSize: '1.2rem',
+        backgroundColor: 'var(--clr-primary-200)',
+        borderBottom: '1px dotted rgba(0,0,0,0.4)',
+        ':hover': {
+            backgroundColor: 'var(--clr-primary-300)',
+            color: 'var(--clr-primary-500)',
+        },
+    }),
+    multiValue: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: 'var(--clr-primary-300)',
+        color: 'var(--clr-primary-500)',
+    }),
+    multiValueLabel: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: 'var(--clr-primary-500)',
+        color: 'var(--clr-primary-100)',
+        padding: '0.3rem',
+        overflow: 'visible'
+    }),
+}
+
+
+export const customColorStyle = {
+    control: styles => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: 'var(--clr-primary-200)',
+        color: 'var(--clr-primary-500)',
+        fontSize: '1.2rem',
+        padding: '0.3rem',
+    }),
+    option: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        fontSize: '1.2rem',
+        backgroundColor: 'var(--clr-primary-200)',
+        color: data.value,
+        borderBottom: '1px dotted rgba(0,0,0,0.4)',
+        ':hover': {
+            backgroundColor: data.value,
+            color: 'var(--clr-primary-500)',
+        },
+    }),
+    multiValue: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: 'var(--clr-primary-300)',
+        color: 'var(--clr-primary-500)',
+
+    }),
+    multiValueLabel: (styles, {data, isDisabled, isFocused, isSelected}) => ({
+        ...styles,
+        fontFamily: 'var(--ff-body-bold)',
+        backgroundColor: data.value,
+        color: 'var(--clr-primary-500)',
+        padding: '0.3rem',
+        overflow: 'visible'
+    }),
+}
