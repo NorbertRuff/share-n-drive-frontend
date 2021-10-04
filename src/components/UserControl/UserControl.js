e1f4d57436f48ce98f49928055172391934e0f0f
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-04 22:52:45 +0200

    [add] user control divs and style

--- src/components/UserControl/UserControl.js
+++ src/components/UserControl/UserControl.js
@@ -1,36 +1,133 @@
 import React, {useState} from 'react';
+import {
+    Container,
+    FormWrap,
+    Ghost,
+    LoginWrapper,
+    Overlay,
+    OverlayContainer,
+    Panel,
+    SocialContainer,
+    UserControlContainer
+} from "./UserControlStyledElements";
+import {FaFacebook, FaGoogle, FaLinkedin} from "react-icons/fa";
+import {NavLink} from "react-router-dom";
 
 
 const UserControl = () => {
-    const [state, setState] = useState({
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState(false)
+    const [fields, setFields] = useState({
         email: "",
-        password: ""
-    })
+        password: "",
+        confirmPassword: "",
+        confirmationCode: "",
+    });
+
+    function validateForm() {
+        return (
+            fields.email.length > 0 &&
+            fields.password.length > 0 &&
+            fields.password === fields.confirmPassword
+        );
+    }
+
+    function handleSubmit() {
+        validateForm();
+    }
+
     const handleChange = (e) => {
         const {id, value} = e.target
-        setState(prevState => ({
+        setFields(prevState => ({
             ...prevState,
             [id]: value
         }))
     }
+
+    // if (loading) {
+    //     return <p>Data is loading...</p>;
+    // }
+    //
+    // if (error) {
+    //     return <ErrorDiv>An error occurred while fetching information. Please try again later!</ErrorDiv>;
+    // }
+
     return (
-        <UserControl>
-            <input type="email"
-                   className="form-control"
-                   id="email"
-                   aria-describedby="emailHelp"
-                   placeholder="Enter email"
-                   value={state.email}
-                   onChange={handleChange}
-            />
-            <input type="password"
-                   className="form-control"
-                   id="password"
-                   placeholder="Password"
-                   value={state.password}
-                   onChange={handleChange}
-            />
-        </UserControl>)
+        <UserControlContainer>
+            <LoginWrapper>
+                <Container>
+                    <FormWrap onSubmit={handleSubmit}>
+                        <h1>Sign in</h1>
+                        <SocialContainer>
+                            <NavLink to="/user" title="User"><FaFacebook/></NavLink>
+                            <NavLink to="/user" title="User"><FaGoogle/></NavLink>
+                            <NavLink to="/user" title="User"><FaLinkedin/></NavLink>
+                        </SocialContainer>
+                        <span>or use your account</span>
+                        <input type="email"
+                               id="email"
+                               aria-describedby="emailHelp"
+                               placeholder="Enter email"
+                               value={fields.email}
+                               onChange={handleChange}
+                        />
+                        <input type="password"
+                               id="password"
+                               placeholder="Password"
+                               value={fields.password}
+                               onChange={handleChange}
+                        />
+                        <NavLink to="/user" title="User">Forgot your password?</NavLink>
+                        <button type={"submit"}>Sign In</button>
+                    </FormWrap>
+                </Container>
+                <Container>
+                    <FormWrap onSubmit={handleSubmit}>
+                        <h1>Create account</h1>
+                        <SocialContainer>
+                            <NavLink to="/user" title="User"><FaFacebook/></NavLink>
+                            <NavLink to="/user" title="User"><FaGoogle/></NavLink>
+                            <NavLink to="/user" title="User"><FaLinkedin/></NavLink>
+                        </SocialContainer>
+                        <span>or use Email</span>
+                        <input type="email"
+                               id="email"
+                               aria-describedby="emailHelp"
+                               placeholder="Enter email"
+                               value={fields.email}
+                               onChange={handleChange}
+                        />
+                        <input type="password"
+                               id="password"
+                               placeholder="Password"
+                               value={fields.password}
+                               onChange={handleChange}
+                        />
+                        <input type="password"
+                               id="password"
+                               placeholder="Confirm Password"
+                               value={fields.password}
+                               onChange={handleChange}
+                        />
+                        <button type={"submit"}>Sign Up</button>
+                    </FormWrap>
+                </Container>
+            </LoginWrapper>
+            <OverlayContainer>
+                <Overlay>
+                    <Panel>
+                        <h1>Welcome Back!</h1>
+                        <p></p>
+                        <Ghost id="signIn">Sign In</Ghost>
+                    </Panel>
+                    <Panel>
+                        <h1>Hello</h1>
+                        <p></p>
+                        <Ghost id="signUp">Sign Up</Ghost>
+                    </Panel>
+                </Overlay>
+            </OverlayContainer>
+        </UserControlContainer>)
 };
 
 export default UserControl;
--- src/components/UserControl/UserControlStyledElements.js
+++ src/components/UserControl/UserControlStyledElements.js
@@ -4,4 +4,98 @@ export const UserControlContainer = styled.div`
   grid-area: MainContent;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoginWrapper = styled.div`
+  background-color: var(--clr-light);
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+  0 10px 10px rgba(0, 0, 0, 0.22);
+  position: relative;
+  width: 60vw;
+  overflow: hidden;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  min-height: 100%;
+  transition: all 0.6s ease-in-out;
+`;
+
+export const SocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+
+
+  > * {
+    padding: 1rem;
+  }
+
+  padding: 1rem;
+`;
+export const FormWrap = styled.form`
+  background-color: #eee;
+  padding: 1rem;
+  margin: 1rem;
+  display: flex;
+  width: 20vw;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  input {
+    background-color: #eee;
+    border: none;
+    padding: 12px 15px;
+    margin: 1rem;
+    width: 100%;
+  }
+
+  button {
+    border-radius: 20px;
+    border: 1px solid var(--clr-primary-500);
+    background-color: var(--clr-primary-300);
+    color: var(--clr-light);
+    font-size: 1rem;
+    font-family: var(--ff-body-bold);
+    padding: 12px 45px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    transition: transform 80ms ease-in;
+  }
+
+  button:active {
+    transform: scale(0.95);
+  }
+
+`;
+
+
+export const OverlayContainer = styled.div``;
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  border: 1px solid var(--clr-primary-200);
+  background-color: var(--clr-primary-300);
+`;
+export const Panel = styled.div``;
+export const Ghost = styled.button`
+  border-radius: 20px;
+  color: var(--clr-light);
+  font-size: 12px;
+  font-family: var(--ff-body-bold);
+  padding: 12px 45px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  background-color: transparent;
+  border-color: var(--clr-light);
 `;
